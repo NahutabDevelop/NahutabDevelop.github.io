b223c3221f738aad9834e9ab9ebd701bd2600199
Nahutab <batuhanaykacswe@gmail.com>
2024-11-15 02:18:01 -0500

    Added some windows 98 css stuff
    Theres a bug with the biography widget. Need to position it absolute to the top left of the screen when maximized.

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,32 +1,77 @@
 // Filename - App.js
 
-import React from "react";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = React.useState(0);
-    return (
-        <div style={{ width: 300 }} className="window">
-        <div className="title-bar">
-            <div className="title-bar-text">Counter</div>
-            <div className="title-bar-controls">
-            <button aria-label="Minimize" />
-            <button aria-label="Maximize" />
-            <button aria-label="Close" />
-            </div>
-        </div>
-
-        <div className="window-body">
-            <p style={{ textAlign: "center" }}>Current count: {count}</p>
-            <div className="field-row" style={{ justifyContent: "center" }}>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <button onClick={() => setCount(0)}>0</button>
-            </div>
-        </div>
-        </div>
-    );
-
+  return (
+    <div className="background-container">
+      <div>
+        <DraggableWindow />
+      </div>
+      <div>
+        <NavigationBar />
+      </div>
+    </div>
+  );
 }
+
+const DraggableWindow = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [isMaximized, setIsMaximized] = useState(false);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  const handleMaximise = () => {
+    setIsMaximized(!isMaximized);
+  };
+
+  return (
+    isVisible && (
+      <Draggable handle=".title-bar" disabled={isMaximized}>
+        <div className={`draggable-window${isMaximized ? `-maximized` : ``}`}>
+          <div
+            className="window"
+            style={{
+              height: "100%",
+              width: "100%",
+            }}
+          >
+            <div className="title-bar">
+              <div className="title-bar-text">
+                <img
+                  class="small-icon-image"
+                  src="/msg_information-0.png"
+                  alt="Biography"
+                ></img>
+                Biography
+              </div>
+              <div className="title-bar-controls">
+                <button aria-label="Minimize"></button>
+                <button aria-label="Maximize" onClick={handleMaximise}></button>
+                <button aria-label="Close" onClick={handleClose}></button>
+              </div>
+            </div>
+            <div className="window-body">
+              <p>Testing out react its kinda fun i guess. </p>
+            </div>
+          </div>
+        </div>
+      </Draggable>
+    )
+  );
+};
+
+const NavigationBar = () => {
+  return (
+    <div
+      className="navbar-container"
+      style={{ position: "absolute", bottom: 0, zIndex: 9999 }}
+    ></div>
+  );
+};
 
 export default App;
